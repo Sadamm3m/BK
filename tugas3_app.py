a4244f424c4cc7c7293bb3fed966db0b317bc10a
sadamm3m <88648410+Sadamm3m@users.noreply.github.com>
2024-01-08 22:27:00 +0700

    Update tugas3_app.py

--- tugas3_app.py
+++ tugas3_app.py
@@ -1,2 +1,11 @@
 import streamlit as st
-st.write("Helo word")
+st.write(
+  "Helo word 1.Pengumpulan data
+   https://archive.ics.uci.edu/dataset/45/heart+disease
+  2.Menelaah Data
+
+  import pandas as pd
+  import re
+  import numpy as np
+  import itertools"
+)
