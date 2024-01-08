52197cccffb52b2bb4f83de382303016b79eb1a3
sadamm3m <88648410+Sadamm3m@users.noreply.github.com>
2024-01-08 22:38:07 +0700

    Update tugas3_app.py

--- tugas3_app.py
+++ tugas3_app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 def main():
-    st.title("Aplikasi Streamlit di VSCode")
+    st.title("Tugas 1")
     
-    long_paragraph = (
-        "Ini adalah paragraf panjang yang berisi banyak teks. "
-        "Anda dapat menambahkan sebanyak yang Anda inginkan "
-        "untuk menampilkan teks dalam satu paragraf."
-    )
+    long_paragraph = """
+    Ini adalah paragraf panjang yang berisi banyak teks. 
+    Anda dapat menambahkan sebanyak yang Anda inginkan 
+    untuk menampilkan teks dalam satu paragraf.
+    """
     
     st.write(long_paragraph)
 
